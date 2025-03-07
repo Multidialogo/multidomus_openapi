c36d955814197ecce7b3e9c48fe29d3e787b1284
MultiDesso <desantis@multicerta.it>
2025-03-07 15:45:46 +0100

    [MD-34] Fix typo

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@
 echo "Getting started"
 
 # Bundle docs into zero-dependency HTML file
-npx redoc-cli bundle openapi/multidoums.yaml && \
+npx redoc-cli bundle ./multidomus.yaml && \
 mv redoc-static.html index.html && \
 echo "Changed name from redoc-static.html to index.html" && \
 # Add favicon
